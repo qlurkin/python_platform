897b8addabb32fb83bfef681ee1a21a88bc8be47
Quentin Lurkin <qlurkin@gmail.com>
2025-06-13 23:27:02 +0200

    Some updates

--- script.js
+++ script.js
@@ -1,24 +1,80 @@
 import * as monaco from "https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/+esm";
 import { asyncRun } from "./workerApi.mjs";
 
-const spinner = document.getElementById("spinner");
-document.body.removeChild(spinner);
-
-const editor_element = document.createElement("div");
-editor_element.id = "editor";
-document.body.appendChild(editor_element);
-
-const myEditor = monaco.editor.create(editor_element, {
-  value: `print('Hello world!')`,
+const myEditor = monaco.editor.create(document.getElementById("editor"), {
+  value: `with open("/home/caca", "w") as file:
+    file.write("merde")`,
   language: "python",
   automaticLayout: true,
 });
 
-const run_btn = document.createElement("button");
-run_btn.innerHTML = "Run";
-document.body.appendChild(run_btn);
-run_btn.addEventListener("click", async () => {
-  const { result, error } = await asyncRun(myEditor.getValue(), {});
-  console.log(result);
-  console.log(error);
+const commands = {
+  python: async () => {
+    const { result, error } = await asyncRun(myEditor.getValue(), {});
+    return { result, error };
+  },
+  cat: async () => {
+    const result = myEditor.getValue();
+    const error = undefined;
+    return { result, error };
+  },
+};
+
+commands.help = async () => {
+  let result = `Available commands:`;
+  for (let cmd in commands) {
+    result += "\n  - " + cmd;
+  }
+  const error = undefined;
+  return { result, error };
+};
+
+const history = [];
+let history_index = 0;
+
+const cmd_input = document.getElementById("cmd");
+
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+cmd_input.addEventListener("keydown", (event) => {
+  if (history.length > 0) {
+    if (event.key === "ArrowUp") {
+      event.preventDefault();
+      history_index -= 1;
+      history_index = mod(history_index, history.length);
+      cmd_input.value = history[history_index];
+    }
+    if (event.key === "ArrowDown") {
+      event.preventDefault();
+      history_index += 1;
+      history_index = mod(history_index, history.length);
+      cmd_input.value = history[history_index];
+    }
+  }
 });
+
+document
+  .getElementById("cmd-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const cmd = data.get("cmd");
+    cmd_input.value = "";
+    console.log(`> ${cmd}`);
+    const index = history.indexOf(cmd);
+    if (index !== -1) {
+      history.splice(index, 1);
+    }
+    history.push(cmd);
+    history_index = 0;
+    if (commands[cmd]) {
+      const { result, error, fs } = await commands[cmd]();
+      if (result) console.log(result);
+      if (error) console.log(error);
+      if (fs) console.log();
+    } else {
+      console.log(`Unknown command "${cmd}"`);
+    }
+  });
--- webworker.mjs
+++ webworker.mjs
@@ -1,21 +1,32 @@
 import { loadPyodide } from "https://cdn.jsdelivr.net/pyodide/v0.27.7/full/pyodide.mjs";
 
-let pyodideReadyPromise = loadPyodide();
+let pyodideReadyPromise = loadPyodide().then((pyodide) => {
+  let mountDir = "/home/pyodide";
+  pyodide.FS.mkdirTree(mountDir);
+  pyodide.FS.mount(pyodide.FS.filesystems.IDBFS, {}, mountDir);
+  return pyodide;
+});
 
 self.onmessage = async (event) => {
   // make sure loading is done
   const pyodide = await pyodideReadyPromise;
   const { id, python, context } = event.data;
-  // Now load any packages we need, run the code, and send the result back.
-  await pyodide.loadPackagesFromImports(python);
-  // make a Python dictionary with the data from `context`
-  const dict = pyodide.globals.get("dict");
-  const globals = dict(Object.entries(context));
-  try {
-    // Execute the python code in this context
-    const result = await pyodide.runPythonAsync(python, { globals });
-    self.postMessage({ result, id });
-  } catch (error) {
-    self.postMessage({ error: error.message, id });
-  }
+  pyodide.FS.syncfs(true, async (err) => {
+    // Now load any packages we need, run the code, and send the result back.
+    await pyodide.loadPackagesFromImports(python);
+    // make a Python dictionary with the data from `context`
+    const dict = pyodide.globals.get("dict");
+    const globals = dict(Object.entries(context));
+    let msg;
+    try {
+      // Execute the python code in this context
+      const result = await pyodide.runPythonAsync(python, { globals });
+      msg = { result, id };
+    } catch (error) {
+      msg = { error: error.message, id };
+    }
+    pyodide.FS.syncfs(false, (err) => {
+      self.postMessage(msg);
+    });
+  });
 };
